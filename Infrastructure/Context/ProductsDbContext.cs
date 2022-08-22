b856e8d941ad3c61ff33b0fdc31c9513587f019b
Dhimas Andrés Angeles King <dhimas.angeles@solvex.com.do>
2022-08-22 15:40:38 -0400

    feat: added file structure.

---
+++ Infrastructure/Context/ProductsDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Context
+{
+    public class ProductsDbContext : BaseDbContext
+    {
+        public ProductsDbContext(DbContextOptions<ProductsDbContext> options) : base(options)
+        {
+            if (Database.ProviderName != "Microsoft.EntityFrameworkCore.InMemory")
+            {
+                Database.Migrate();
+            }
+        }
+        public DbSet<User> Users { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+
+}
